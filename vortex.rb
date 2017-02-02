10024c2a295a8d83c4c0b1e1fea9599ed421783e
Liam Schumm <liamschumm@icloud.com>
2017-02-01 23:26:02 -0600

    Add vortex.rb formula

---
+++ vortex.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Ergonomica < Formula
+  homepage 'https://github.com/ergonomica/package-vortex'
+  url 'https://github.com/ergonomica/package-vortex.git'
+
+  version "1.0.0-beta.1"
+
+  def install
+
+    system "cp", "vortex.py", "~/.ergo/"
+    
+  end
+end
